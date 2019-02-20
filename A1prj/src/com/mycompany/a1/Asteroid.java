cabcae6d1a92efd634604c8d6c4096ca59209bb7
Jose <flores11@csus.edu>
2019-02-20 12:27:38 -0800

    complete

--- A1prj/src/com/mycompany/a1/Asteroid.java
+++ A1prj/src/com/mycompany/a1/Asteroid.java
@@ -1,5 +1,26 @@
 package com.mycompany.a1;
 
 public class Asteroid extends MovableGameObject {
-
+	private int size;
+	
+	public Asteroid(){
+		super(ColorUtil.BLACK);
+		final int MIN_SIZE=6;
+		final int MAX_SIZE=30;
+		this.size = GameOBject.rand.netInt(MAX_SIZE-MIN_SIZE+1)+MIN_SIZE;
+	}
+	
+	public int getSize(){
+		return this.size;
+	}
+	
+	public String toString(){
+		return (
+			"Astroid : loc="+GameObject.round(getX()) + "," + GameObject.round(getY()) +
+			" Color="  + GameObject.getColorString(getColor())+
+			" Speed="  + GameObject.round(getSpeed()) +
+			" dir="    + getDirection()+
+			" Size="   + this.getSize()
+				);
+	}
 }
