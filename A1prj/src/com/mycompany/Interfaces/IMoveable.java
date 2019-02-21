c521771fe93349429a667f83320144995abc2bdc
Jose <flores11@csus.edu>
2019-02-20 19:14:45 -0800

    .

--- A1prj/src/com/mycompany/Interfaces/IMoveable.java
+++ A1prj/src/com/mycompany/Interfaces/IMoveable.java
@@ -2,11 +2,6 @@ package com.mycompany.Interfaces;
 
 public interface IMoveable {
 
-<<<<<<< HEAD
 	void move();
 	
-=======
-	void move(int amount);
-
->>>>>>> branch 'master' of https://github.com/Josetf2396/CSC133.Assignment1.git
 }
