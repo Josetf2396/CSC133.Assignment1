8b6dc6b21658fb900a135bba1b651e5fc0d0a729
Jose <flores11@csus.edu>
2019-02-20 19:05:38 -0800

    .

--- A1prj/src/com/mycompany/Interfaces/IMoveable.java
+++ A1prj/src/com/mycompany/Interfaces/IMoveable.java
@@ -2,13 +2,6 @@ package com.mycompany.Interfaces;
 
 public interface IMoveable {
 
-	void move(int amount);
-	int getX();
-	int getY();
-	int getSpeed();
-	int getDirection();
-	void setX(int x);
-	void setY(int x);
-	void setSpeed(int Speed);
+	void move();
 	
 }
--- A1prj/src/com/mycompany/Interfaces/ISteerable.java
+++ A1prj/src/com/mycompany/Interfaces/ISteerable.java
@@ -2,7 +2,6 @@ package com.mycompany.Interfaces;
 
 public interface ISteerable {
 
-	void turnLeft(int degrees);
-	void turnRight(int degrees);
-	int getCurrentDirection();
+	void turnLeft();
+	void turnRight();
 }
