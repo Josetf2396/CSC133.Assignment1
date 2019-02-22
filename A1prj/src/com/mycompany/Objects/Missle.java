8b93afdee6128719b4e41d4e2cbfb428fb9971f7
Jose <joseflores2396@gmail.com>
2019-02-21 16:36:24 -0800

    -implmented missle object

--- A1prj/src/com/mycompany/Objects/Missle.java
+++ A1prj/src/com/mycompany/Objects/Missle.java
@@ -4,9 +4,34 @@ import com.mycompany.GameObjects.MoveableGameObject;
 
 public class Missle extends MoveableGameObject {
 
+	private int fuel;
 	
+	public Missle(int speed, int direction, float x, float y) {
+		this.fuel = 10;
+		super.setSpeed(speed + 10);
+		super.setDirection(direction);
+		super.setX(x);
+		super.setY(y);
+	}
+	
+	public void move() {
+		this.fuel -= 1;
+		super.move();
+
+	}
+
+	public int getFuelLevel() {
+		return this.fuel;
+	}
+	
+	public void setFuelLevel(int fuel) {
+		this.fuel = fuel;
+	}
+
 	public String toString(){
-		return(".");
+		return 
+				"Missiles: " + super.toString() + 
+				"fuel=" + this.fuel; 
 	}
 
 }
