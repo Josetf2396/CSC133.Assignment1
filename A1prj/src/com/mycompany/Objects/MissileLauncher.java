33a2f73c7eeabb7dd419e9372c9cb04502b80066
Jose <flores11@csus.edu>
2019-02-25 23:48:00 -0800

    Progress on missleLauncher

--- A1prj/src/com/mycompany/Objects/MissileLauncher.java
+++ A1prj/src/com/mycompany/Objects/MissileLauncher.java
@@ -4,15 +4,27 @@ import com.mycompany.ObjectTypes.SteerableGameObject;
 
 
 public class MissileLauncher extends SteerableGameObject {
-
+	private int missilecount = 10;
+	private static MissileLauncher weapon;
 	
-	public MissileLauncher(int direction, float x, float y) {
+	public MissileLauncher() {
 		super.setDirection(direction);
 		super.setX(x);
 		super.setY(y);
 	}
-	public void fire(){
-		
+	
+	public static MissileLauncher getLauncher() {
+		if (weapon == null) {
+			weapon = new MissileLauncher();
+		}
+		return weapon;
+	}
+	public void setMissiles(int inMissiles) {
+		this.missilecount += inMissiles;
+	}
+
+	public int getMissiles() {
+		return this.missilecount;
 	}
 
 	
